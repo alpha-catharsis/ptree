79f74cb06cd5e86b5386ced8be204a9944eb73be
Alpha Catharsis <alpha.catharsis@gmail.com>
2022-10-09 07:03:52 +0200

    Performed a small refactoring

--- ptree.el
+++ ptree.el
@@ -40,11 +40,13 @@
 
 ;;; Code:
 
-;; Public functions
+;; Public constructor functions
 
 (defun ptree-create ()
   "Create an empty property tree."
   (list nil nil nil))
+
+;; Public predicates
 
 (defun ptree-root-p (node)
   "Return t if NODE is the root of the property tree, otherwise nil."
@@ -58,6 +60,8 @@
   "Return t if NODE is a leaf of the property tree, otherwise nil."
   (null (cddr node)))
 
+;; Public accessors
+
 (defun ptree-get-node-tag (node)
   "Return the tag associated with the NODE."
   (car node))
@@ -66,13 +70,6 @@
   "Return the value associated with the NODE."
   (cadr node))
 
-(defun ptree-set-node-value (node value)
-  "Set value of NODE to VALUE.
-An error is returned if the node is not a leaf."
-  (if (ptree-leaf-p node)
-      (setcdr node (list value))
-    (error "Node is not a leaf")))
-
 (defun ptree-get-child-nodes-num (node)
   "Return the number of child nodes of NODE."
   (let ((child-nodes (cddr node)))
@@ -80,62 +77,76 @@ An error is returned if the node is not a leaf."
         0
       (length (cddr node)))))
 
-(defun ptree-add-node-at-path (pt path)
-  "Add PATH in tree PT.
-If part of the path already exist, it is not modified. If the path runs
-through a leaf, it is turned into a branch to complete the specified path.
-The function returns the last node of the path."
-  (let* ((path-list (ptree--get-path-as-list path))
-        (res (ptree--create-path pt path-list)))
-    (ptree--make-empty-branch res)
-    res))
+(defun ptree-get-child-node-at-index (node index)
+  "Get child node at INDEX in NODE. Return nil if index is out of range."
+  (nth index (cddr node)))
 
-(defun ptree-add-child-nodes (pt &rest branches)
-  "Add BRANCHES in tree PT.
-If the branch already exists, is in not modified. It the brach has the
- same tag of an existing leaf, the leaf is turned into a branch."
-  (while branches
-    (ptree--make-empty-branch
-     (ptree--create-node pt (car branches)))
-    (setq branches (cdr branches))))
-
-(defun ptree-add-value-at-path (pt value path)
-  "Add a leaf with VALUE in PT at PATH the specified path.
-If part of the path already exist, it is not modified. If the path runs
-through a leaf, it is turned into a branch to complete the specified path.
-The function returns the leaf node."
-  (let* ((path-list (ptree--get-path-as-list path))
-         (res (ptree--create-path pt path-list)))
-    (ptree--make-leaf res value)
-    res))
-
-(defun ptree-get-node-at-path (tree path)
-  "Get node at PATH in TREE. Return nil if the node does not exist."
-  (let ((path-list (ptree--get-path-as-list path))
-        (node tree))
+(defun ptree-get-node-at-path (node path)
+  "Get node at PATH in NODE. Return nil if the node does not exist."
+  (let ((path-list (ptree--get-path-as-list path)))
     (while (and node path-list)
       (setq node (ptree--get-node node (car path-list)))
       (setq path-list (cdr path-list)))
     node))
 
-(defun ptree-get-child-node-at-index (tree index)
-  "Get child node at INDEX in TREE. Return nil if index is out of range."
-  (nth index (cddr tree)))
-
-(defun ptree-get-value-at-path (tree path)
-  "Get the value of leaf at PATH in TREE.
+(defun ptree-get-value-at-path (node path)
+  "Get the value of leaf at PATH in NODE.
 Return 'not-found if the node does not exist or if it is not a leaf."
-  (let* ((path-list (ptree--get-path-as-list path))
-         (node (ptree-get-node-at-path tree path-list)))
+  (let* ((path-list (ptree--get-path-as-list path)))
+    (setq node (ptree-get-node-at-path node path-list))
     (if (and node (ptree-leaf-p node))
         (ptree-get-node-value node)
       'not-found)))
 
-(defun ptree-delete-node-at-path (tree path)
-  "Delete node at PATH in TREE.
+;; Public mutatorsb
+
+(defun ptree-set-node-value (node value)
+  "Set value of NODE to VALUE.
+An error is returned if the node is not a leaf."
+  (if (ptree-leaf-p node)
+      (setcdr node (list value))
+    (error "Node is not a leaf")))
+
+(defun ptree-add-child-nodes (node &rest branches)
+  "Add BRANCHES in tree NODE.
+If the branch already exists, is in not modified. It the brach has the
+ same tag of an existing leaf, the leaf is turned into a branch."
+  (while branches
+    (ptree--make-empty-branch
+     (ptree--create-node node (car branches)))
+    (setq branches (cdr branches))))
+
+(defun ptree-add-node-at-path (node path)
+  "Add PATH in tree NODE.
+If part of the path already exist, it is not modified. If the path runs
+through a leaf, it is turned into a branch to complete the specified path.
+The function returns the last node of the path."
+  (let* ((path-list (ptree--get-path-as-list path))
+        (res (ptree--create-path node path-list)))
+    (ptree--make-empty-branch res)
+    res))
+
+(defun ptree-add-value-at-path (node value path)
+  "Add a leaf with VALUE in NODE at PATH the specified path.
+If part of the path already exist, it is not modified. If the path runs
+through a leaf, it is turned into a branch to complete the specified path.
+The function returns the leaf node."
+  (let* ((path-list (ptree--get-path-as-list path))
+         (res (ptree--create-path node path-list)))
+    (ptree--make-leaf res value)
+    res))
+
+(defun ptree-delete-child-nodes (node &rest child-nodes)
+  "Delete the specified CHILD-NODES from NODE."
+  (while child-nodes
+    (if (not (ptree--delete-node node (car child-nodes)))
+        (error "Path does not exist"))
+    (setq child-nodes (cdr child-nodes))))
+
+(defun ptree-delete-node-at-path (node path)
+  "Delete node at PATH in NODE.
 Return the deleted node. Throw a an error if the node does not exist"
-  (let ((path-list (ptree--get-path-as-list path))
-        (node tree))
+  (let ((path-list (ptree--get-path-as-list path)))
     (while (and node (cdr path-list))
       (setq node (ptree--get-node node (car path-list)))
       (setq path-list (cdr path-list)))
@@ -146,14 +157,15 @@ Return the deleted node. Throw a an error if the node does not exist"
             deleted-node
           (error "Path does not exist"))))))
 
-(defun ptree-delete-child-nodes (tree &rest child-nodes)
-  "Delete the specified CHILD-NODES from TREE."
-  (while child-nodes
-    (if (not (ptree--delete-node tree (car child-nodes)))
-        (error "Path does not exist"))
-    (setq child-nodes (cdr child-nodes))))
-
 ;; Internal functions
+
+(defun ptree--make-empty-branch (node)
+  "Transform the NODE into an empty tree."
+  (setcdr node (list nil nil)))
+
+(defun ptree--make-leaf (node value)
+  "Transform the NODE into a leaf with VALUE."
+  (setcdr node (list value)))
 
 (defun ptree--get-path-as-list (path)
   "Get PATH as list."
@@ -185,9 +197,9 @@ Return the deleted node. Throw a an error if the node does not exist"
                           ((string= ltag rtag) 'eq)
                           (t 'gt)))))))
 
-(defun ptree--create-node (tree tag)
-  "Get or insert node with TAG in TREE."
-  (let* ((cns (cddr tree))
+(defun ptree--create-node (node tag)
+  "Get or insert node with TAG in NODE."
+  (let* ((cns (cddr node))
          (cn (car cns))
          (res nil))
     (while (not res)
@@ -197,15 +209,15 @@ Return the deleted node. Throw a an error if the node does not exist"
                    (setq cn (car cns)))
           (setq res (list tag))
           (cond ((eq cmp 'gt) (setcdr cns (list res)))
-                ((eq cmp 'empty) (setcdr tree (list nil res)))
+                ((eq cmp 'empty) (setcdr node (list nil res)))
                 ((eq cmp 'eq) (setq res cn))
                 (t (setcdr cns (cons cn (cddr cns)))
                    (setcar cns res))))))
     res))
 
-(defun ptree--get-node (tree tag)
-  "Get node with TAG in TREE."
-  (let* ((cns (cddr tree))
+(defun ptree--get-node (node tag)
+  "Get node with TAG in NODE."
+  (let* ((cns (cddr node))
          (cn (car cns))
          (res nil))
     (while (and cn (not res))
@@ -215,17 +227,16 @@ Return the deleted node. Throw a an error if the node does not exist"
         (setq cn (car cns))))
     res))
 
-(defun ptree--create-path (tree path-list)
-  "Create PATH-LIST in TREE."
-  (let ((node tree))
-    (while path-list
-      (setq node (ptree--create-node node (car path-list)))
-      (setq path-list (cdr path-list)))
-    node))
+(defun ptree--create-path (node path-list)
+  "Create PATH-LIST in NODE."
+  (while path-list
+    (setq node (ptree--create-node node (car path-list)))
+    (setq path-list (cdr path-list)))
+  node)
 
-(defun ptree--delete-node (tree tag)
-  "Delete node with TAG in TREE."
-  (let* ((cns (cddr tree))
+(defun ptree--delete-node (node tag)
+  "Delete node with TAG in NODE."
+  (let* ((cns (cddr node))
          (pcns nil)
          (cn (car cns))
          (res nil))
@@ -237,21 +248,13 @@ Return the deleted node. Throw a an error if the node does not exist"
                                        (progn (setcar cns (cadr cns))
                                               (setcdr cns (cddr cns)))
                                      (if (eq pcns nil)
-                                         (setcdr tree (list nil nil))
+                                         (setcdr node (list nil nil))
                                        (setcdr pcns nil)))))
               ((eq cmp 'gt) (progn (setq pcns cns)
                                    (setq cns (cdr cns))
                                    (setq cn (car cns))))
               (t (setq cn nil)))))
     res))
-
-(defun ptree--make-empty-branch (tree)
-  "Transform the TREE into an empty tree."
-  (setcdr tree (list nil nil)))
-
-(defun ptree--make-leaf (tree value)
-  "Transform the TREE into a leaf with VALUE."
-  (setcdr tree (list value)))
 
 ;; Package provision
 
